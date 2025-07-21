952f9d6c7d38a33e132fe2ea2a9f1c053a51cef7
Anjali Thakur <anjalithakur1824@gmail.com>
2025-07-21 17:01:40 +0530

    Add decrementQuantity reducer

--- ReduxToolkit/src/Features/CartSlice.js
+++ ReduxToolkit/src/Features/CartSlice.js
@@ -12,11 +12,21 @@ const cartSlice = createSlice({
         state.push({ ...action.payload, quantity: 1 });
       }
     },
+    decrementQuantity: (state, action) => {
+      const existingItem = state.find(item => item.id === action.payload);
+      if (existingItem) {
+        if (existingItem.quantity > 1) {
+          existingItem.quantity -= 1;
+        } else {
+          return state.filter(item => item.id !== action.payload);
+        }
+      }
+    },
     removeFromCart: (state, action) => {
       return state.filter(item => item.id !== action.payload);
     },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart,decrementQuantity, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
